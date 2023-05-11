132f1af9d0947eb9723040f5b6ba150fff1b3896
MatheusBalestreiro <matheusafonso95@gmail.com>
2023-05-11 17:19:16 -0300

    Configurando Stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
+import { styled } from "../styles"
+
+const Button = styled('button',{
+  backgroundColor: '$rocketseat'
+})
+
 export default function Home() {
   return (
-    <h1>Hello word</h1>
+    <Button>Hello word</Button>
   )
 }
