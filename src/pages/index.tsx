bbc66ba78859ba607c2d95c4adafe2c12cc3bb0f
MatheusBalestreiro <matheusafonso95@gmail.com>
2023-05-12 15:30:53 -0300

    Estrutura da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
-import { styled } from "../styles"
+import { HomeContainer, Product } from "@/styles/pages/home"
+import Image from "next/image"
 
-const Button = styled('button',{
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px'
-})
+import camiseta1 from '../assets/camisetas/1.png'
 
 export default function Home() {
   return (
-    <Button>Hello word</Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt=''/>
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt=''/>
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
