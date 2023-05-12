1e732c1c22202af7e635dcd81af6af791918b06c
MatheusBalestreiro <matheusafonso95@gmail.com>
2023-05-12 14:28:53 -0300

    Cabeçalho da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,18 @@ import { globalStyles } from "@/styles/global";
 import { AppProps } from "next/app";
 
 import logoImg from '../assets/logo.svg'
+import { Container, Header } from "@/styles/pages/app";
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps){
   return (
-    <div>
-      <header>
+    <Container>
+      <Header>
         <img src={logoImg.src} />
-      </header>
+      </Header>
       <Component {...pageProps} />
-    </div>
+    </Container>
     
   )
 }
